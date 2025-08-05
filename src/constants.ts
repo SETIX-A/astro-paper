4dd7664af51bc736c19012b75e4bff8fa5408ba4
SETIX-A <socrates.02zx@gmail.com>
2025-08-05 15:33:08 +0800

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -19,25 +19,19 @@ interface Social {
 export const SOCIALS: Social[] = [
   {
     name: "GitHub",
-    href: "https://github.com/satnaing/astro-paper",
+    href: "https://github.com/SETIX-A/astro-paper",
     linkTitle: `${SITE.title} on GitHub`,
     icon: IconGitHub,
   },
   {
     name: "X",
-    href: "https://x.com/username",
+    href: "https://x.com/SETIX_A",
     linkTitle: `${SITE.title} on X`,
     icon: IconBrandX,
   },
   {
-    name: "LinkedIn",
-    href: "https://www.linkedin.com/in/username/",
-    linkTitle: `${SITE.title} on LinkedIn`,
-    icon: IconLinkedin,
-  },
-  {
     name: "Mail",
-    href: "mailto:yourmail@gmail.com",
+    href: "Socrates.02zx@Gmail.com",
     linkTitle: `Send an email to ${SITE.title}`,
     icon: IconMail,
   },
